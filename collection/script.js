40075aa96f537634e5421dfd6a31edc7364ae66d
web-coder <yasher18@bk.ru>
2024-10-18 13:25:02 +0300

    изучал классы в js

--- collection/script.js
+++ collection/script.js
@@ -48,3 +48,62 @@ for (let all of Object.entries(person5)) {
 console.log("-------------------------")
 console.log(Object.keys(person5))
 console.log(Object.values(person5))
+
+class Coder {
+    constructor(options) {
+        this.name = options.name
+        this.age = options.age 
+        this.lp = options.lp
+    }
+
+    hello() {
+        console.log(`Hello, my name is ${this.name} me ${this.age} years and I am ${this.lp} coder!`)
+    }
+}
+
+let lps = ["JavaScript", "TypeScript", "html/css"]
+let ages = [19, 23, 24]
+let names = ["web-coder", "typerOne", "htmler"]
+let coders = []
+
+for (let i = 0; i < lps.length; i++) {
+    let newCoder = new Coder({
+        name: names[i],
+        age: ages[i],
+        lp: lps[i]
+    })
+    coders.push(newCoder)
+}
+
+coders.forEach(men => men.hello())
+
+console.log("-------------------------")
+
+class WebCoder extends Coder {
+    constructor(options) {
+        super(options)
+        this.end = options.end
+    }
+    hello2() {
+        console.log(`i am ${this.end} coder on ${this.lp}`)
+    }
+}
+
+let webCoders = []
+let lps2 = [
+    { "lp": "php", "end": "backend" },
+    { "lp": "nodeJS", "end": "fullstack" },
+    { "lp": "java", "end": "backend" },
+    { "lp": "swift", "end": "ios" },
+    { "lp": "kotlin", "end": "android" }
+]
+
+for (let i = 0; i < lps2.length; i++) {
+    let newWebCoder = new WebCoder({
+        lp: lps2[i].lp,
+        end: lps2[i].end
+    })
+    webCoders.push(newWebCoder)
+}
+
+webCoders.forEach(assassa => assassa.hello2())
